989ef0de3ff53019d582728e789870ecb42ada84
Daniel Sitek <dan.sitek@gmail.com>
2018-01-22 16:28:10 +0100

    updated to bigint cuz int sometimes caused error "out of range value for column"

--- db.sql
+++ db.sql
@@ -1,17 +1,16 @@
+
 CREATE TABLE auctions (
-    auc int(15),
-    item int(15),
-    owner varchar(22),
-    buyout int(15),
-    quantity int(15), 
+    auc bigint(20),
+    item bigint(20),
+    owner varchar(40),
+    buyout bigint(20),
+    quantity bigint(20),
     PRIMARY KEY (auc)
 );
 
 CREATE TABLE status (
     realm varchar(21)
-
 );
 
 INSERT INTO status
 VALUES (123);
-

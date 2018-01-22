15b13329c3935538feb1f94f17cd93bb33113abc
Daniel Sitek <dan.sitek@gmail.com>
2018-01-22 16:29:20 +0100

    config moved to separate file with definitions

---
+++ default.config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Default Config file.
+ *
+ * Before start, duplicate this file to `config.php` and edit it.
+ */
+
+define( 'SERVER_NAME', 'localhost' );       // Database server name
+define( 'DB_USER_NAME', 'dbUser' );         // Database user name
+define( 'DB_USER_PASS', 'dbPassword' );     // Database user password
+define( 'DB_SCHEMA', 'dbName' );            // Database name
+
+define( 'BATTLE_API_KEY', 'yourAPIKey' );   // Get your API key from https://dev.battle.net.
+define( 'REALM_NAME', 'slugRealmName' );    // Slug realm name
+define( 'REALM_REGION', 'RealmRegion' );    // Realm region. eu|us
+define( 'LOCALE', 'en_GB' );                // Battle.net API locale
